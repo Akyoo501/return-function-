fccffbaa8e293e5c0b9906aee8c30c5056e40f91
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-15 19:24:36 +0300

    Add files via upload

---
+++ return function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int add(int x, int y);
+
+int main(){
+	
+	cout<<"Result is: "<<add(2,3);
+	
+	
+	return 0;
+}
+
+int add(int x, int y){
+	 int z=x+y;
+	return z;
+	
+}
